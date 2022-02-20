32bd8a0105ff7a323deaa321d0d2ad1bd0fcc6ff
Robert Warren <warrenrobert127@gmail.com>
2022-02-20 11:17:06 -0600

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,32 @@ var getCurrentDate = function () {
   var todaysDate = moment().format("dddd, MMMM Do");
   console.log(todaysDate);
   $("#currentDay").text(todaysDate);
-};
+
 
 //retrieve hour and text from local storage
+$(".hour-block").each(function () {
+        var id = $(this).attr("id");
+        var text = localStorage.getItem(id);
+
+        if (text !== null) {
+            $(this).children(".text").val(text);
+        }
+    });
+};
+
 getCurrentDate();
 
-$("hour-block").each(function(index){
-    console.log(index)
-})
+// add local storage
+var saveBtn = $(".saveBtn")
+
+$(".saveBtn").click(function(){
+    var hour = $(this).parent().attr("id")
+    console.log(hour)
+    var text = $(this).siblings(".text").val()
+    console.log(text)
+    localStorage.setItem(hour, text);
+
+    })
 var hour = document.getElementById(".hour-block")
 
 getCurrentDate();
